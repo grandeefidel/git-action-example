89824e816abf98c7d2de4bf92e69bb1a5542b7ef
grandeefidel <grandeefidel@gmail.com>
2023-01-06 15:03:22 +0100

    Ãinit commit

---
+++ src/main/java/com/fidel/gitaction/TestController.java
@@ -0,0 +1,16 @@
+package com.fidel.gitaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping()
+    public String welcome(){
+        return "Welcome to Test Application";
+    }
+
+}
